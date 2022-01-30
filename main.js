535fb874ea50dedb76324742e73c67265d080a88
FakeBigBoi <nickernoose@gmail.com>
2022-01-30 13:49:13 -0500

    nav animation

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 $(window).on("load", function() {
     $('.preloader').css('display', 'none')
+    if (navTop > landingEnd) {
+        $('.logo').addClass('hidden')
+        $('.contact').addClass('hidden')
+        $('.name').addClass('hidden')
+    } else {
+        $('.logo').removeClass('hidden')
+        $('.contact').removeClass('hidden')
+        $('.name').removeClass('hidden')
+    }
 })
 
-function copy(args) {
-    navigator.clipboard.writeText(args);
-}
+$(window).scroll(function() {
+    var landingEnd = $('.landing').position().top + $('.landing').height();
+    var navTop = $(document).scrollTop();
+
+    if (navTop > landingEnd) {
+        $('.logo').addClass('hidden')
+        $('.contact').addClass('hidden')
+        $('.name').addClass('hidden')
+    } else {
+        $('.logo').removeClass('hidden')
+        $('.contact').removeClass('hidden')
+        $('.name').removeClass('hidden')
+    }
+})
+
