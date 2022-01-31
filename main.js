408545780090ff4a52b3ae5b3cf281fd62bcded7
FakeBigBoi <41173009+FakeBigBoi@users.noreply.github.com>
2022-01-31 12:26:01 -0500

    js

--- main.js
+++ main.js
@@ -1,68 +1,19 @@
 $(window).on("load", function() {
     $('.preloader').css('display', 'none')
-    var landingEnd = $('.landing').position().top + $('.landing').height();
-    var navTop = $(document).scrollTop();
-    if (navTop > landingEnd) {
-        $('.logo').addClass('hidden')
-        $('.contact').addClass('hidden')
-        $('.name').addClass('hidden')
-    } else {
-        $('.logo').removeClass('hidden')
-        $('.contact').removeClass('hidden')
-        $('.name').removeClass('hidden')
-    }
-    introLoop(true)
 })
 
-
-    
-$(document).on("click", "#4", function(){
-    if ($('#4').hasClass('show')) {
-        $('#4').removeClass('show')
-        introLoop(false)
-    }
-    });
-
-function introLoop(initial) {
-    if (initial == false) {
-        var dlay = 1100
-    } else {
-        var dlay = 5100
-    }
-    setTimeout(function() {
-        $('.slogan #1').addClass('show')
-        setTimeout(function() {
-            $('.slogan #1').removeClass('show')
-            setTimeout(function() {
-                $('.slogan #2').addClass('show')
-                setTimeout(function() {
-                    $('.slogan #2').removeClass('show')
-                    setTimeout(function() {
-                        $('.slogan #3').addClass('show')
-                        setTimeout(function() {
-                            $('.slogan #3').removeClass('show')
-                            setTimeout(function() {
-                                $('.slogan #4').addClass('show')
-                            }, 1100)
-                        }, 2250)
-                    },1100)
-                }, 2250)
-            },1100)
-        }, 2250)
-    }, dlay)
+function copy(args) {
+    navigator.clipboard.writeText(args);
 }
-$(window).scroll(function() {
-    var landingEnd = $('.landing').position().top + $('.landing').height();
-    var navTop = $(document).scrollTop();
 
-    if (navTop > landingEnd) {
-        $('.logo').addClass('hidden')
-        $('.contact').addClass('hidden')
-        $('.name').addClass('hidden')
-    } else {
-        $('.logo').removeClass('hidden')
-        $('.contact').removeClass('hidden')
-        $('.name').removeClass('hidden')
-    }
-})
-
+var lastScroll = 0;
+$('body').scroll(function()
+{
+var nowScrollTop = $(this).scrollTop();
+if (nowScroll > lastScroll){
+    $('nav').addClass('hidden')
+} else if (nowScroll < lastScroll) {
+    $('nav').addClass('hidden')
+}
+lastScroll = nowScroll;
+});
